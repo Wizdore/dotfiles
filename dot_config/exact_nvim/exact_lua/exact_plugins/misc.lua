b10ce5d711aa3cdec6db30b25e8e1426dde3b330
Wizdore <wizdore@gmail.com>
2024-11-01 21:44:36 +0100

    updated nix, added dap

--- dot_config/exact_nvim/exact_lua/exact_plugins/misc.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/misc.lua
@@ -11,27 +11,36 @@ return {
       local dap = require("dap")
       local dapui = require("dapui")
       dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
     end
   },
   {
     "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require "dap"
+      local dapui = require("dapui")
+      
+      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<leader>df', dap.run_to_cursor)
+      vim.keymap.set('n', '<leader>dc', dap.continue)
+      vim.keymap.set('n', '<leader>dr', dap.restart)
+      vim.keymap.set('n', '<leader>dsi', dap.step_into)
+      vim.keymap.set('n', '<leader>dso', dap.step_out)
+      vim.keymap.set('n', '<leader>dui', dapui.toggle)
+
+      vim.keymap.set('n', '<leader>dv', function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+    end
   },
   {
     "mfussenegger/nvim-dap-python",
     ft = "python",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
     },
     config = function(_, opts)
       local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
