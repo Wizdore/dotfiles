86d281db61c74e4a0a6493329cf2b11c7307f17a
Wizdore <wizdore@gmail.com>
2024-12-06 02:55:14 +0100

    added tailscale configuration

--- dot_config/exact_nix/exact_modules/network.nix
+++ dot_config/exact_nix/exact_modules/network.nix
@@ -2,4 +2,12 @@
 {
   networking.networkmanager.enable = true;
   networking.hostName = "capgemini";
+  
+  services.tailscale.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [ 41641 51820 ];
+    trustedInterfaces = [ "tailscale0" "wg0" ];
+  };
 }
