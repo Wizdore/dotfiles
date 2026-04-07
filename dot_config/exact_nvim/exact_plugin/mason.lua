93cafa1e32a853684e37bd1948f0c27a68b0cb18
Wizdore <wizdore@gmail.com>
2026-04-08 00:31:19 +0200

    added registry for roslyn lsp to mason

--- dot_config/exact_nvim/exact_plugin/mason.lua
+++ dot_config/exact_nvim/exact_plugin/mason.lua
@@ -1,11 +1,15 @@
-vim.pack.add({"https://github.com/mason-org/mason.nvim",})
+vim.pack.add({ "https://github.com/mason-org/mason.nvim" })
 
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	},
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
