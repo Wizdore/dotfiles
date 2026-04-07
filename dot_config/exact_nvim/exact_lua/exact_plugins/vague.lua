7f1b7c2a25121850ae27a110fac1ed1a1c9c2986
Wizdore <wizdore@gmail.com>
2026-04-08 00:31:18 +0200

    cleaning up old config structure

--- dot_config/exact_nvim/exact_lua/exact_plugins/vague.lua
+++
@@ -1,9 +0,0 @@
-require("vague").setup({
-	italic = false,
-	on_highlights = function(groups, colors)
-		groups.Comment = { fg = colors.comment, italic = true }
-		groups["@comment"] = { fg = colors.comment, italic = true }
-		groups["@keyword"] = { fg = colors.keyword, italic = true }
-	end,
-})
-vim.cmd.colorscheme("vague")
