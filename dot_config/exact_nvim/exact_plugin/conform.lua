26ad2417ac88bf97392b149996f20398ed93765a
Wizdore <wizdore@gmail.com>
2026-05-13 14:51:14 +0200

    added biome

--- dot_config/exact_nvim/exact_plugin/conform.lua
+++ dot_config/exact_nvim/exact_plugin/conform.lua
@@ -3,24 +3,30 @@ vim.pack.add({
 })
 
 require("conform").setup({
-  formatters_by_ft = {
-    lua        = { "stylua" },
-    rust       = { "rustfmt" },
-    typescript = { "prettier" },
-    javascript = { "prettier" },
-    typescriptreact = { "prettier" },
-    javascriptreact = { "prettier" },
-    json       = { "prettier" },
-    python     = { "ruff_format" },
-  },
+	formatters_by_ft = {
+		lua             = { "stylua" },
+		rust            = { "rustfmt" },
+		typescript      = { "biome" },
+		javascript      = { "biome" },
+		typescriptreact = { "biome" },
+		javascriptreact = { "biome" },
+		json            = { "biome" },
+		python          = { "ruff_format" },
+	},
 
-  format_on_save = {
-    timeout_ms = 2000,
-    lsp_fallback = true, -- fall back to LSP formatter if no conform formatter matches
-  },
+	formatters = {
+		rustfmt = {
+			args = { "--emit=stdout", "--edition=2024" },
+		},
+	},
+
+	format_on_save = {
+		timeout_ms = 2000,
+		lsp_fallback = true, -- fall back to LSP formatter if no conform formatter matches
+	},
 })
 
 -- Optional: manual format keymap
 vim.keymap.set({ "n", "v" }, "<leader>f", function()
-  require("conform").format({ async = true, lsp_fallback = true })
+	require("conform").format({ async = true, lsp_fallback = true })
 end)
